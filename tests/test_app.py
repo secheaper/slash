4fe6842f51160d48c8fdc9936e718e1353fb4a0a
Shubham Mankar <themrshubh@gmail.com>
2021-09-22 20:08:47 -0400

    Updated buildResult test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import scraper
 
 def test_buildResult():
-    assert scraper.buildResult([1,2,3]) == 1
+    assert scraper.buildResult([1,2,3]) == [1,2]
 
 def test_formatSearchQuery():
     assert scraper.formatSearchQuery("1 2") == "1+2"
