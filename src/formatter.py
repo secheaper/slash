9c04e4fd7849ac38bcef8abd22e5fe3abe49a6ca
Shubham Mankar <themrshubh@gmail.com>
2021-09-28 10:57:54 -0400

    Added comments to formatter.py

--- src/formatter.py
+++ src/formatter.py
@@ -1,7 +1,17 @@
+"""
+The formatter module focuses on processing raw text and returning it in 
+the required format. 
+"""
+
 from datetime import datetime
 import math
 
 def formatResult(website, titles, prices, links):
+    """
+    The formatResult function takes the scraped HTML as input, and extracts the 
+    necessary values from the HTML code. Ex. extracting a price '$19.99' from
+    a paragraph tag.
+    """
     title, price, link = '', '', ''
     if titles: title = titles[0].get_text().strip()
     if prices: price = prices[0].get_text().strip()
@@ -16,22 +26,38 @@ def formatResult(website, titles, prices, links):
     return product
 
 def sortList(arr, sortBy, reverse):
+    """
+    The sortList function is used to sort the products list based on the
+    flags provided as args. Currently, it supports sorting by price.
+    """
     if sortBy == "pr":
         return sorted(arr, key=lambda x: getNumbers(x["price"]), reverse=reverse)
+    # To-do: sort by rating
     elif sortBy == "ra":
         # return sorted(arr, key=lambda x: getNumbers(x.price), reverse=reverse)
         pass
     return arr
 
 def formatSearchQuery(query):
+    """
+    The formatSearchQuery function formats the search string into a string that 
+    can be sent as a url paramenter.
+    """
     return query.replace(" ", "+")
 
 def formatTitle(title):
+    """
+    The formatTitle function formats titles extracted from the scraped HTML code.
+    """
     if(len(title) > 40):
         return title[:40] + "..."
     return title
 
 def getNumbers(st):
+    """
+    The getNumbers function extracts float values (price) from a string.
+    Ex. it extracts 10.99 from '$10.99' or 'starting at $10.99'
+    """
     ans = ''
     for ch in st:
         if (ch >= '0' and ch <= '9') or ch == '.':
