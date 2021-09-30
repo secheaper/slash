4f8418f1d12ddebf260c259b1047873475536457
moksh98 <mokshjain98@gmail.com>
2021-09-29 20:22:49 -0400

    Added copyrights for source folder codes

--- src/formatter.py
+++ src/formatter.py
@@ -1,4 +1,13 @@
 """
+Copyright (C) 2021 SE Slash - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the MIT license.
+You should have received a copy of the XYZ license with
+this file. If not, please write to: secheaper@gmail.com
+
+"""
+
+"""
 The formatter module focuses on processing raw text and returning it in 
 the required format. 
 """
