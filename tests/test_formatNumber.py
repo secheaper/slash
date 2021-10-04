be98a3d1a08a8cba73dc70d3e9f8f11cd6525f2a
Rahil Sarvaiya <rahil1304@gmail.com>
2021-10-04 10:10:27 -0400

    added MIT license typo fix

--- tests/test_formatNumber.py
+++ tests/test_formatNumber.py
@@ -2,7 +2,7 @@
 Copyright (C) 2021 SE Slash - All Rights Reserved
 You may use, distribute and modify this code under the
 terms of the MIT license.
-You should have received a copy of the XYZ license with
+You should have received a copy of the MIT license with
 this file. If not, please write to: secheaper@gmail.com
 
 """
