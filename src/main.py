947218379d4215f939c170ce86d9f846430766e0
apande <ani.pande1995@gmail.com>
2021-10-18 20:05:44 -0400

    Fast API
    Fast API main driver

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from typing import Optional
+import scraper
+from fastapi import FastAPI
+import json 
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+
+
