9f9d734e7e008ba418f1b9d557c744967277260b
apande <ani.pande1995@gmail.com>
2021-10-31 01:45:10 -0400

    FastAPI
    FastAPI changes -- baseline

--- src/main.py
+++ src/main.py
@@ -1,28 +1,50 @@
+# package imports
 from typing import Optional
-import scraper
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import json 
 import logging
+
+# local imports
+import src.scraper.scraper as scr
+from src.scraper.configs import AMAZON, WALMART
+
 # Open function to open the file "MyFile1.txt"  
 # (same directory) in read mode and 
-
-
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    '''Get documentation of API
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    Parameters
+    ----------
+    None
+
+    Returns
+    ----------
+    documentation redirect
+    '''
+    response = RedirectResponse(url='/redoc')
+    return response
 
 @app.get("/az/{item_name}")
-async def searchAmazonAPI(item_name: str, q: Optional[str] = None):
+async def search_Amazon_API(item_name: str, q: Optional[str] = None): 
+    '''Wrapper API to fetch AMAZON, WALMART and TARGET query results
+
+    Parameters
+    ----------
+    item_name: string of item to be searched
+
+    Returns
+    ----------
+    itemListJson: JSON List
+        list of search results as JSON List
+    '''
     file1 = open("logger.txt", "a")
     file1.write('amazon query:' + str(item_name)+'\n')
-    itemList = scraper.searchAmazon(item_name)
+    itemList = scr.search(query = item_name,config= AMAZON)
     itemListJson = json.dumps(itemList) 
     file1.close()
     return itemListJson
