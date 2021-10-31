99b4c753dbf84aa1911613adcb1acd29730c0b29
apande <ani.pande1995@gmail.com>
2021-10-31 14:01:24 -0400

    Uvicorn Server

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 # package imports
+import uvicorn
+from os import scandir
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
@@ -6,11 +8,9 @@ import json
 import logging
 
 # local imports
-import src.scraper.scraper as scr
-from src.scraper.configs import AMAZON, WALMART
+import scraper.scraper as scr
+from scraper.configs import AMAZON, WALMART
 
-# Open function to open the file "MyFile1.txt"  
-# (same directory) in read mode and 
 
 app = FastAPI()
 
@@ -26,11 +26,11 @@ async def read_root():
     ----------
     documentation redirect
     '''
-    response = RedirectResponse(url='/redoc')
+    response = RedirectResponse(url='/docs')
     return response
 
-@app.get("/az/{item_name}")
-async def search_Amazon_API(item_name: str, q: Optional[str] = None): 
+@app.get("/{site}/{item_name}")
+async def search_Amazon_API(site : str,item_name: str, q: Optional[str] = None): 
     '''Wrapper API to fetch AMAZON, WALMART and TARGET query results
 
     Parameters
@@ -42,12 +42,37 @@ async def search_Amazon_API(item_name: str, q: Optional[str] = None):
     itemListJson: JSON List
         list of search results as JSON List
     '''
-    file1 = open("logger.txt", "a")
-    file1.write('amazon query:' + str(item_name)+'\n')
-    itemList = scr.search(query = item_name,config= AMAZON)
+    #logging in file
+    file = open("logger.txt", "a")
+    file.write('amazon query:' + str(item_name)+'\n')
+    
+    #building argument 
+    args = {
+    'search': item_name,
+    'sort' : 'pr', # placeholder TDB
+    'des' : 'True' # placeholder TBD
+    }
+
+    scrapers = []
+
+    if site == 'az' or site == 'all':
+        scrapers.append('amazon')
+    if site == 'wm' or site == 'all':
+        scrapers.append('walmart')
+    if site == 'tg' or site == 'all':
+        scrapers.append('target')
+
+    #calling scraper.scrape to fetch results
+    itemList =  scr.scrape(args = args, scrapers= scrapers)
+ 
+    #returning JSON list
     itemListJson = json.dumps(itemList) 
-    file1.close()
+    file.close()
     return itemListJson
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8822)
+
 
 
 
